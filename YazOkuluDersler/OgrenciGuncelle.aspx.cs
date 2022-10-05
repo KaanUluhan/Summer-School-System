3caf74b4f6483888d72247ff7a06da460db4d592
Kaan <kaanuluhan23@gmail.com>
2022-10-05 23:20:04 +0300

    system that can be used by students who are in summer school

---
+++ YazOkuluDersler/OgrenciGuncelle.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EntityLayer;
+using DataAccessLayer;
+using BusinessLogicLayer;
+
+namespace YazOkuluDersler
+{
+    public partial class OgrenciGuncelle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+            int x = Convert.ToInt32(Request.QueryString["OGRID"].ToString());
+            Txtıd.Text = x.ToString();
+            Txtıd.Enabled = false;
+
+            if(Page.IsPostBack == false) { 
+
+            EntityOgrenci ent = new EntityOgrenci();
+            List<EntityOgrenci> OgrList = BLLOgrenci.BllDetay(x);
+            TxtAd.Text = OgrList[0].AD.ToString();
+            TxtSoyad.Text = OgrList[0].SOYAD.ToString();
+            TxtNumara.Text = OgrList[0].NUMARA.ToString();
+            TxtFoto.Text = OgrList[0].FOTOGRAF.ToString();
+            TxtSifre.Text = OgrList[0].SIFRE.ToString();
+
+        }
+}
+        protected void TxtSifre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            EntityOgrenci ent = new EntityOgrenci();
+            ent.AD = TxtAd.Text;
+            ent.SOYAD = TxtSoyad.Text;
+            ent.SIFRE = TxtSifre.Text;
+            ent.NUMARA = TxtNumara.Text;
+            ent.FOTOGRAF = TxtFoto.Text;
+            ent.ID = Txtıd.Text;
+            BLLOgrenci.OgrenciGuncelleBLL(ent);
+            Response.Redirect("OgrenciListesi.Aspx");
+        }
+
+        protected void Txtıd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Txtıd_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ YazOkuluDersler/OgrenciGuncelle.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace YazOkuluDersler
+{
+
+
+    public partial class OgrenciGuncelle
+    {
+
+        /// <summary>
+        /// Form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
+
+        /// <summary>
+        /// Txtıd denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txtıd;
+
+        /// <summary>
+        /// TxtAd denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtAd;
+
+        /// <summary>
+        /// TxtSoyad denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSoyad;
+
+        /// <summary>
+        /// TxtNumara denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNumara;
+
+        /// <summary>
+        /// TxtSifre denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSifre;
+
+        /// <summary>
+        /// TxtFoto denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtFoto;
+
+        /// <summary>
+        /// Button1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
